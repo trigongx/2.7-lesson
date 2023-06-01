243885e81212c365cca5283784a18827aabb6f1b
trigongx <bolotbekovdaniar7@gmail.com>
2023-06-01 16:45:08 +0600

    commit

--- app/src/main/java/com/example/a27_lesson/MainActivity.java
+++ app/src/main/java/com/example/a27_lesson/MainActivity.java
@@ -2,12 +2,14 @@ package com.example.a27_lesson;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnSeeResult;
     private TextView textView;
     private Boolean isOperationClick;
     private Double first,second,result;
@@ -17,11 +19,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView = findViewById(R.id.tv_result);
+        btnSeeResult = findViewById(R.id.btn_see_result);
+        btnSeeResult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ResultActivity.class);
+                intent.putExtra("result",result.toString());
+                startActivity(intent);
+            }
+        });
+        btnSeeResult.setVisibility(View.GONE);
+
     }
     public void onNumberClick(View view){
         Button button = (Button) view;
         String number = button.getText().toString();
         setNumber(number);
+        btnSeeResult.setVisibility(View.GONE);
     }
     public void setNumber(String number){
         if(textView.getText().toString().equals("0")){
@@ -34,10 +48,15 @@ public class MainActivity extends AppCompatActivity {
         isOperationClick = false;
     }
     public void onOperationClick(View view){
+        btnSeeResult.setVisibility(View.GONE);
         if (view.getId() == R.id.btn_ac){
             textView.setText("0");
             first = 0.0;
             second = 0.0;
+        }
+        else if(view.getId() == R.id.btn_ac){
+            textView.setText("0");
+            first = 0.0;
         }
         else if (view.getId() == R.id.btn_plus) {
             operation = "+";
@@ -46,6 +65,7 @@ public class MainActivity extends AppCompatActivity {
         }
         else if (view.getId() == R.id.btn_equal) {
             second = Double.parseDouble(textView.getText().toString());
+            btnSeeResult.setVisibility(View.VISIBLE);
             if(operation.equals("+")){
                 result = first+second;
                 textView.setText(cancelDouble(result));
@@ -86,6 +106,11 @@ public class MainActivity extends AppCompatActivity {
         }
         else if (view.getId() == R.id.btn_percent) {
             operation = "%";
+            first = Double.parseDouble(textView.getText().toString());
+            isOperationClick = true;
+        }
+        else if (view.getId() == R.id.btn_erase) {
+            operation = "<";
             first = Double.parseDouble(textView.getText().toString());
             isOperationClick = true;
         }
